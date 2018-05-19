a284483112ece6c5f735f9d1bd32c2b5bf074427
Student <student@Admins-MacBook-Air.local>
2018-05-19 13:08:24 -0700

    starts search

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-/* write your script here */ 
+/* write your script here */
+let database,
+
+    searchBar,
+
+    searchButton,
+
+    autoSuggestions,
+
+    display;
+
+
+database = [{
+
+  country: "France",
+
+  invention: "Syncronization Gear",
+
+  inventor: "Anthony Fokker",
+
+  Uses:
+
+}];
+
+searchBar = document.getElementById("search-bar");
+
+searchButton = document.getElementById("search-button");
+
+autoSuggestions = document.getElementById("auto-suggestions");
+
+display = document.getElementById("display");
+
+
+
+searchBar.addEventListener("keypress",checkKey);
+
+serachButton.addEventListener("click",processInput);
+
+
+function checkKey(e) {
+
+  varkey = e.which || e.keycode;
+
+  if (key == 13) {
+
+    console.log("you pressed enter!!");
+
+    processInput();
+
+  }
+
+}
+
+
+function processInput(){
+  cleanInput =
+}
+
+function getRecord(cleanedInput){
+  for(let i = 0;i<database.length;i++){
+    let cleanedRecordName = database[i]
+  }
+}
